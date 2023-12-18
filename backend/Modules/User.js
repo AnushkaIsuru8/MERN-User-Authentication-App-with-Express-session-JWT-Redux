49a951b6226000855f5da596198fdcec9c7177e8
AnushkaIsuru8 <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-12-18 23:27:16 +0530

    Trting to Connect MongoDB
    MongooseServerSelectionError: connect ECONNREFUSED ::1:27017
        at _handleConnectionErrors (B:\Projects\MERN_Sign-Up_and_Sign-In\backend\node_modules\mongoose\lib\connection.js:792:11)
        at NativeConnection.openUri (B:\Projects\MERN_Sign-Up_and_Sign-In\backend\node_modules\mongoose\lib\connection.js:767:11)
        at runNextTicks (node:internal/process/task_queues:60:5)
        at listOnTimeout (node:internal/timers:540:9)
        at process.processTimers (node:internal/timers:514:7) {
      reason: TopologyDescription {
        type: 'Unknown',
        servers: Map(1) { 'localhost:27017' => [ServerDescription] },
        stale: false,
        compatible: true,
        heartbeatFrequencyMS: 10000,
        localThresholdMS: 15,
        setName: null,
        maxElectionId: null,
        maxSetVersion: null,
        commonWireVersion: 0,
        logicalSessionTimeoutMinutes: null
      },
      code: undefined
    }

--- backend/Modules/User.js
+++ backend/Modules/User.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username:String,
-    password:String,
+    username:{type:String, required:true},
+    password:{type:String, required:true},
     role:{
         type:String,
         default:'guest'
     }
 })
 
-const UserModel = mongoose.model.apply("User", UserSchema)
+const UserModel = mongoose.model("User", UserSchema)
 
 module.exports = UserModel
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const UserModel = require('./Modules/User')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(8080, () => {
-    console.log('Server Listning')
+mongoose.connect('mongodb://localhost:27017/M')
+.then(()=>{
+    console.log('db cpneccted')
+}).catch(err =>{console.log(err)})
+
+app.get('/fgd', (req,res) =>{
+    UserModel.find().then(function (users){
+        res.json(users)
+    }).catch(err => {console.log(err)})
 })
 
-mongoose.connect('mongodb://localhost:27017/test1')
-
-app.post('/', (req,res) =>{
-    res.json(req.body)
+app.listen(8080, () => {
+    console.log('Server Listning')
 })
