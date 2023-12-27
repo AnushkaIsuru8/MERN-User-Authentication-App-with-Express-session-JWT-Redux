24cf4a0f7b7aabc1b699e88137c2a57429d8dffe
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-12-27 21:32:37 +0530

    SignUP suer Controller

---
+++ backend/userController.js
@@ -0,0 +1,19 @@
+const User = require('./Modules/User')
+
+const signUp = async (req, res, next) => {
+    const [username, Password] = req.body
+    const user = new User({
+        username,
+        password
+    })
+
+    try{
+        await user.save()
+    }catch(err){
+        console.log(err)
+    }
+
+    return res.status(201).json({message:"SAVED "})
+}
+
+exports.signUp
