60ecfa518f51e3d559bc5dffef1624bd1acce935
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-12-28 23:58:47 +0530

    Update UserModel

--- backend/Modules/User.js
+++ backend/Modules/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
     password:{type:String, required:true},
     role:{
         type:String,
+        enum:['admin', 'guest'],
         default:'guest'
     }
 })
