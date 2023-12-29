3937247cc04e3860b311c066ead815678d3680ec
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-12-29 14:10:51 +0530

    Fix  Can't access req.body
    use
    "app.use(express.json())"
    before
    app.use('/api', router)

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const router = require('./userRouter')
+
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', router)
 
 mongoose.connect('mongodb://0.0.0.0/test')
 .then(()=>{
     console.log('db Connected')
 }).catch(err =>{console.log(err)})
-
-
-app.use('/', router)
-
-app.use(express.json())
 
 app.listen(8080, ()=>{
     console.log("Listning")
