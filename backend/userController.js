17231bdbcc1041d8175faa859244460137a65e00
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2023-12-30 14:16:52 +0530

    Hashing Password

--- backend/userController.js
+++ backend/userController.js
@@ -1,32 +1,34 @@
 const User = require('./Modules/User')
-
+const bcrypt = require('bcrypt')
 const signUp = async (req, res, next) => {
-    
+
     const { username, password } = req.body;
-    
+
     let alreadySaved
-    try{
-        alreadySaved = await User.findOne({username:username})
-    }catch (err){
+    try {
+        alreadySaved = await User.findOne({ username: username })
+    } catch (err) {
         console.log(err)
     }
 
-    if(alreadySaved){
-        return res.status(400).json({message : "already have that username"})
+    if (alreadySaved) {
+        return res.status(400).json({ message: "already have that username" })
     }
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
 
     const user = new User({
         username,
-        password
+        password: hashedPassword
     })
 
-    try{
+    try {
         await user.save()
-    }catch(err){
+    } catch (err) {
         console.log(err)
     }
 
-    return res.status(201).json({message:"SAVED "})
+    return res.status(201).json({ message: "SAVED " })
 }
 
 exports.signUp = signUp
