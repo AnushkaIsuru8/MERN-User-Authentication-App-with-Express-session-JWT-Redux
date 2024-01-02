d099139869dff7b2e79fa6ab7df5b2fb04de4e83
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-02 10:39:07 +0530

    Lerning Code move to Folder

---
+++ backend/LearningCodes/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const router = require('./userRouter')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
+const app = express()
+app.use(cookieParser())
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', router)
+
+mongoose.connect(process.env.MONGODB_CONNECTIN_STRING)
+.then(()=>{
+    console.log('db Connected')
+}).catch(err =>{console.log(err)})
+
+app.listen(8080, ()=>{
+    console.log("Listning")
+})
