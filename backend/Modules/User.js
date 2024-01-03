9524819269e346a38f54ffb52de5048aaacd0985
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-03 22:07:13 +0530

    Update User.js

--- backend/Modules/User.js
+++ backend/Modules/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-    username:{type:String, required:true},
+    username:{type:String, required:true, unique : true},
     password:{type:String, required:true},
     role:{
         type:String,
-        enum:['admin', 'guest','signed'],
-        default:'guest'
+        enum:['0', '1','2'],
+        default:'1'
     }
 })
 
