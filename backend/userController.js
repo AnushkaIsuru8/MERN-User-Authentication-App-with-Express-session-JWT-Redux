c181502186e7bf44036c5bfb77e4345e3be5c615
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-03 17:13:05 +0530

    verifyToken, getUser, clearCookie

--- backend/userController.js
+++ backend/userController.js
@@ -71,7 +71,7 @@ const loginSuccessfull = async (req, res, next) => {
     }
   );
 
-  res.cookie(userID, token, {
+  res.cookie("userID", token, {
     path: "/",
     httpOnly: true,
     expires: new Date(Date.now() + 1000 * 60),
@@ -81,7 +81,51 @@ const loginSuccessfull = async (req, res, next) => {
   return res.status(200).json({ message: "Login Successfull" })
 }
 
+const verifyToken = async (req, res, next) => {
+  const token = req.headers.cookie.split("userID=")[1]
+  if (!token) {
+    return res.status(404).json({ message: "No token found" })
+  }
+
+  jwt.verify(String(token), process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      console.log(err)
+      return res.status(400).json({ message: "Invalid token" })
+    } else {
+      req.id = user.id
+      next()
+    }
+  })
+}
+
+const getUser = async (req, res, next) => {
+  let user
+  try {
+    user = await User.findById(req.id)
+  } catch (eer) {
+    console.log(err)
+  }
+
+  if (user) {
+    return res.status(202).json({
+      username: user.username,
+      role: user.role,
+      userId: user._id
+    })
+  } else {
+    return res.status(401).json({ message: "Invalid" })
+  }
+}
+
+const clearCookie2 = async (req, res, next) => {
+  //res.clearCookie("connect.sid");
+  return res.status(200).json({ message: "clear" })
+}
+
 exports.setUsername = setUsername;
 exports.register = register;
 exports.login = login;
 exports.loginSuccessfull = loginSuccessfull
+exports.verifyToken = verifyToken
+exports.getUser = getUser
+exports.clearCookie2 = clearCookie2
