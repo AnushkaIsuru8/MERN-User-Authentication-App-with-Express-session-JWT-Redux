d0b12e918803c95a9b186e89b6b7ad66c4ea4c4e
Anushka Isuru <129645615+AnushkaIsuru8@users.noreply.github.com>
2024-01-04 08:14:13 +0530

    GIve Access to frontend auth

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,10 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    credentials:true,
+    origin: "http://localhost:3000"
+}))
 app.use(express.json())
 app.use(session({
     secret: "Some secret",
